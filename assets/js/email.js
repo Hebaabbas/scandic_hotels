28d34b5869df245a9de37306c02d13038758f1ee
Hebaabbas <heba1996abbas@gmail.com>
2023-09-01 15:06:12 +0000

    fix the email javascript code

--- assets/js/email.js
+++ assets/js/email.js
@@ -9,8 +9,8 @@ var templateParams = {
  
 emailjs.send('service_u77afju', 'template_mageqwt', templateParams, "rbSvDk825LGhLJpd6")
     .then(function(response) {
-       console.log('Your message was sent successfully', response.status, response.text);
+       console.log('Success', response.status, response.text);
     }, function(error) {
-       console.log('Please try again', error);
+       console.log('Failure', error);
     });
 }
